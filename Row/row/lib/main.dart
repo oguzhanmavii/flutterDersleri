c77731a1e438d62ad447d640bb5319096cf59935
OĞUZHAN MAVİ <77650437+oguzhanmavii@users.noreply.github.com>
2021-12-29 20:10:12 +0300

    flutterDersleri
    Başlangıç Düzeyinden İleri Düzeye Kadar Flutter Konu Anlatımı ve İçerikleri

--- Row/row/lib/main.dart
+++ Row/row/lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -9,70 +9,58 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: MyColumn(),
+      home: MyRow(),
     );
   }
 }
 
-class MyColumn extends StatefulWidget {
-  const MyColumn({Key? key}) : super(key: key);
+class MyRow extends StatefulWidget {
+  const MyRow({Key? key}) : super(key: key);
 
   @override
-  _MyColumnState createState() => _MyColumnState();
+  _MyRowState createState() => _MyRowState();
 }
 
-class _MyColumnState extends State<MyColumn> {
+class _MyRowState extends State<MyRow> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Center(child: Text("Column")),
+        title: Center(child: const Text("Row")),
       ),
-      body: Column(
+      body: Row(
         crossAxisAlignment: CrossAxisAlignment.stretch,
         mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
           Container(
-            width: 50,
-            height: 50,
             color: Colors.red,
-            child: Center(child: Text("k1")),
-          ),
-          Container(
             width: 50,
             height: 50,
-            color: Colors.yellow,
-            child: Center(child: Text("k2")),
+            child: Text("K1"),
           ),
           Container(
-            width: 50,
-            height: 50,
             color: Colors.greenAccent,
-            child: Center(child: Text("k3")),
-          ),
-          Container(
             width: 50,
             height: 50,
+            child: Text("K2"),
+          ),
+          Container(
+            color: Colors.yellow,
+            width: 50,
+            height: 50,
+            child: Text("K3"),
+          ),
+          Container(
             color: Colors.blue,
-            child: Center(child: Text("k4")),
-          ),
-          Container(
             width: 50,
             height: 50,
-            color: Colors.green,
-            child: Center(child: Text("k5")),
+            child: Text("K4"),
           ),
           Container(
-            width: 50,
-            height: 50,
             color: Colors.purple,
-            child: Center(child: Text("k6")),
-          ),
-          Container(
             width: 50,
             height: 50,
-            color: Colors.lime,
-            child: Center(child: Text("k7")),
+            child: Text("K5"),
           ),
         ],
       ),
